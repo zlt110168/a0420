cbb13b95ff6acc364951d21f6208df9642e61d7b
zlt11068 <870195909@qq.com>
2024-04-21 23:20:01 +0800

    部署的远程

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,30 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import {VantResolver} from "@vant/auto-import-resolver";
+import { VantResolver } from "@vant/auto-import-resolver";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue(), react()],
   prefetch: {
-    defaultStrategy: "load",
+    defaultStrategy: "load"
   },
   build: {
-    format: "file",
+    format: "file"
   },
   vite: {
-    plugins: [
-      AutoImport({
-        resolvers: [VantResolver()],
-      }),
-      Components({
-        resolvers: [VantResolver()],
-      }),
-    ],
+    plugins: [AutoImport({
+      resolvers: [VantResolver()]
+    }), Components({
+      resolvers: [VantResolver()]
+    })]
   },
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel()
 });
